6e0724cdbcb36a3c35eee066c917004a08c79875
WonJongWoon <delphi3228@pukyong.ac.kr>
2021-05-21 23:23:57 +0900

    Modify Some Code in 2230 Problem

--- 2230.cpp
+++ 2230.cpp
@@ -8,14 +8,9 @@
 #define SIZE (100000+2)
 
 using namespace std;
-using ll = long long;
 
 int N, M;
 int arr[SIZE];
-
-bool is_possible(int l, int m) {
-    return arr[m] - arr[l] >= M;
-}
 
 int main(void) {
     ios::sync_with_stdio(false);
@@ -29,10 +24,7 @@ int main(void) {
     for(int i = 0; i < N; i++) {
         int base = arr[i];
         int* p1 = lower_bound(arr, arr + N, base + M);
-        //int* p2 = lower_bound(arr, arr + N, base - M);
-
         if (p1 != arr + N && (*p1 - base) >= M) min_diff = min(min_diff, *p1 - base);
-        // if ((base - *p2) >= M) min_diff = min(min_diff, base - *p2);
     }
 
     cout << min_diff << "\n";
